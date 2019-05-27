40c96cffd9ac5fcacac61e8c674855b8b0c8b9df
Juan Fernando Rangel Ramirez <rangel.juanfernando@gmail.com>
2019-05-27 15:53:14 +0100

    update

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ To create our database user and the database for our webpage:
 
 1.- CREATE ROLE "postgres" with Login Superuser Createrole CreateDB Replication BypassRLS;
 2.- CREATE DATABASE "postgres";
-3.- CREATE DATABASE "auth-system"
+3.- CREATE DATABASE "auth-system";
 
 -- SERVER --
 
 1.- npm install
-2.- node server.js
+2.- node index.js
--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 /* Website Server with Express */
 "use strict"
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const morgan = require('morgan');
+const User = require('./models/user');
 const path = require('path');
-const app = express();
-const fs = require('fs'); // Is this the promises version
-
-/* External Requests */
 const http = require('http');
 const url = require('url'); // Do we need this
-const OK = 200, NotFound = 404, BadType = 415, Error = 500;
-const request = require('request-promise'); /* Do we need this? */
+const helmet = require('helmet');
+const banned = [];
+const fs = require('fs');
+banUpperCase("./public/", "");
+var options = { setHeaders: deliverXHTML };
+// invoke an instance of express application.
+const app = express();
+
+// set our application port
+app.set('port', 8080);
+
+// set morgan to log info about our requests for development use.
+app.use(morgan('dev'));
+
+// initialize body-parser to parse incoming parameters requests to req.body
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// initialize cookie-parser to allow us access the cookies stored in the browser.
+app.use(cookieParser());
 
 /* Handles the requests for specific files */
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,26 +39,158 @@ app.use((req,res,next) => { // Middleware
   next(error); // Will forward the error request
 })
 
-app.use((error, req, res, next) => {// This will handle errors from everywhere (including failed opeartions on the database)
-  res.status(error.status || 500).send('Page Not Found'); // Either specified error code, or 500
+ // Handle 500
+ app.use(function (err, req, res, next) {
+   console.error(err.stack)
+   res.status(500).send('500: Internal Server error');
+ });
+
+// initialize express-session to allow us track the logged-in user across sessions.
+app.use(session({
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}));
+
+
+// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
+// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');        
+    }
+    next();
 });
 
 
+// middleware function to check for logged-in users
+var sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.redirect('/dashboard');
+    } else {
+        next();
+    }    
+};
+
+
+// route for Home-Page
+app.get('/', sessionChecker, (req, res) => {
+    res.redirect('/login');
+});
+
+
+// route for user signup
+app.route('/signup')
+    .get(sessionChecker, (req, res) => {
+        res.sendFile(__dirname + '/public/signup.html');
+    })
+    .post((req, res) => {
+        User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        .then(user => {
+            req.session.user = user.dataValues;
+            res.redirect('/dashboard');
+        })
+        .catch(error => {
+            res.redirect('/signup');
+        });
+    });
+
+
+// route for user Login
+app.route('/login')
+    .get(sessionChecker, (req, res) => {
+        res.sendFile(__dirname + '/public/login.html');
+    })
+    .post((req, res) => {
+        var username = req.body.username,
+            password = req.body.password;
+
+        User.findOne({ where: { username: username } }).then(function (user) {
+            if (!user) {
+                res.redirect('/login');
+            } else if (!user.validPassword(password)) {
+                res.redirect('/login');
+            } else {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            }
+        });
+    });
+
+
+// route for user's dashboard
+app.get('/dashboard', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.sendFile(__dirname + '/public/dashboard.html');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+// route for user logout
+app.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.clearCookie('user_sid');
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+// route for handling 404 requests(unavailable routes)
+app.use(function (req, res, next) {
+  res.status(404).send("404: Page Cannot be Found");
+})
+
+/* URL must be lowercase */
+function lower(req, res, next) {
+    req.url = req.url.toLowerCase();
+    next();
+}
+
+/* Banned URLS cannot gain access */
+function ban(req, res, next) {
+    for (var i=0; i<banned.length; i++) {
+        var b = banned[i];
+        if (req.url.startsWith(b)) {
+            res.status(404).send("Filename not lower case");
+            return;
+        }
+    }
+    next();
+}
+
+function deliverXHTML(res, path, stat) {
+    if (path.endsWith(".html")) {
+        res.header("Content-Type", "application/xhtml+xml");
+    }
+}
+
+/* Prevents the use of UpperCase files */
+function banUpperCase(root, folder) {
+    var folderBit = 1 << 14;
+    var names = fs.readdirSync(root + folder);
+    for (var i=0; i<names.length; i++) {
+        var name = names[i];
+        var file = folder + "/" + name;
+        if (name != name.toLowerCase()) banned.push(file.toLowerCase());
+        var mode = fs.statSync(root + file).mode;
+        if ((mode & folderBit) == 0) continue;
+        banUpperCase(root, file);
+    }
+}
+
+
+// start the express server
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-                                 /* Tasks  */
-// Implement content negociation to support old browsers
-// Support HTTPs?
-// HTTP Status codes
-// Check supported filetypes with express
-/* Use Middleware functions -- Have access to server request and response */
-/* Use to serve static files to either general or specific requests  */
-
-// A user sends and request information through routes
-// We need to use get requests to return HTML, CSS, IMAGES ect (if someone goes
-// to /flower, then execute a specific function )
-
-// Middleware forwards requests to other files which may contain a router
-// If you get pass the static folder that means it didnt have anything to handle
-// your request so capture it
+app.listen(PORT, 'localhost', () => console.log(`Server started on port ${PORT}`));
